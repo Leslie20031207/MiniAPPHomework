d3ec97abc1383279a25d4a57aac8ca0c05be6a5b
王晓婷 <11648598+leslie2003@user.noreply.gitee.com>
2025-01-10 21:02:25 +0800

    Initial commit

---
+++ pages/home/home.vue
@@ -0,0 +1,312 @@
+<template>
+	<view class="container">
+		<swiper class="banner-swiper" 
+				circular 
+				autoplay 
+				:interval="3000" 
+				:duration="800"
+				:indicator-dots="true"
+				indicator-active-color="#12c2e9">
+			<swiper-item class="banner-swiper-item" v-for="(item, index) in swipers" :key="index">
+				<image :src="item" mode="aspectFill" class="swiper-image"></image>
+			</swiper-item>
+		</swiper>
+		<view class="content">
+			<!-- section-1 begin -->
+			<view class="section-1">
+				<view class="grid-container">
+					<navigator class="grid-item" open-type="switchTab" url="/pages/index/index" hover-class="none">
+						<view class="icon-wrapper">
+							<image src="/static/images/home/home_icon_ziqu1.png" mode="aspectFit"></image>
+						</view>
+						<view class="grid-text">
+							<text class="title">到店取</text>
+							<text class="subtitle">提前下单面排队</text>
+						</view>
+					</navigator>
+					
+					<navigator class="grid-item" open-type="navigate" url="/pages/addresses/addresses" hover-class="none">
+						<view class="icon-wrapper">
+							<image src="/static/images/home/home_icon_waimai1.png" mode="aspectFit"></image>
+						</view>
+						<view class="grid-text">
+							<text class="title">喜外送</text>
+							<text class="subtitle">随时喜送到家</text>
+						</view>
+					</navigator>
+				</view>
+			</view>
+			<!-- section-2 end -->
+			<!-- section-2 begin -->
+			<view class="section-2">
+				<view class="card" @click="navigateToMart">
+					<view class="card-content">
+						<view class="card-title">
+							<image src="/static/images/home/home_icon_baihuo1.png" mode="aspectFit"></image>
+							<text>喜茶百货</text>
+						</view>
+						<text class="card-desc">精选好物 为你而选</text>
+					</view>
+					<view class="card-bg bg-1"></view>
+				</view>
+				
+				<view class="card" @click="navigateToGroupDinner">
+					<view class="card-content">
+						<view class="card-title">
+							<image src="/static/images/home/home_icon_qiye1.png" mode="aspectFit"></image>
+							<text>企业团餐</text>
+						</view>
+						<text class="card-desc">企业专享优惠</text>
+					</view>
+					<view class="card-bg bg-2"></view>
+				</view>
+			</view>
+			<!-- section-2 end -->
+			<view class="section-3">
+				<view class="points-card">
+					<view class="points-info">
+						<text class="points-label">我的积分</text>
+						<text class="points-value">63</text>
+					</view>
+					<text class="points-desc">可兑换多款精美礼品</text>
+					<view class="points-btn">立即使用</view>
+				</view>
+				
+				<view class="qr-card">
+					<image src="/static/images/home/home_icon_erweima.png" mode="aspectFit"></image>
+					<text>会员码</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				swipers: [
+					"https://go.cdn.heytea.com/storage/ad/2020/05/21/bfd57914d80d4671b19f5d0ccc176cd5.jpg",
+					"https://go.cdn.heytea.com/storage/ad/2020/05/24/49f9b50738dd47878cf1ab54c2eee2e0.jpg",
+					"https://go.cdn.heytea.com/storage/ad/2020/05/01/e1c6915022c849fd9492377021aef454.jpg",
+					"https://go.cdn.heytea.com/storage/ad/2020/04/27/db60b797c1cd4afabe9666e7df958ffd.jpg",
+					"https://go.cdn.heytea.com/storage/ad/2020/05/21/4ba53dda599345bdaf28a08420dd1b6f.jpg"
+				]
+			}
+		},
+		methods: {
+			navigateToMart() {
+				uni.switchTab({
+					url: '/pages/mart/mart'
+				})
+			},
+			navigateToGroupDinner() {
+				uni.navigateTo({
+					url: '/pages/home/group-dinner'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	page {
+		max-height: 100%;
+	}
+	
+	.banner-swiper {
+		width: 100%;
+		height: 420rpx;
+		
+		.swiper-image {
+			width: 100%;
+			height: 100%;
+			border-bottom-left-radius: 30rpx;
+			border-bottom-right-radius: 30rpx;
+		}
+	}
+	
+	.content {
+		margin-top: 40rpx;
+		padding: 20rpx;
+	}
+	
+	.section-1 {
+		margin-top: -40rpx;
+		padding: 20rpx;
+		background: #fff;
+		border-radius: 20rpx;
+		box-shadow: 0 4rpx 20rpx rgba(0, 0, 0, 0.08);
+		
+		.grid-container {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			gap: 20rpx;
+		}
+		
+		.grid-item {
+			display: flex;
+			align-items: center;
+			padding: 20rpx;
+			background: linear-gradient(135deg, #f6f8fb 0%, #f0f2f5 100%);
+			border-radius: 16rpx;
+			
+			.icon-wrapper {
+				width: 80rpx;
+				height: 80rpx;
+				margin-right: 20rpx;
+				
+				image {
+					width: 100%;
+					height: 100%;
+				}
+			}
+			
+			.grid-text {
+				display: flex;
+				flex-direction: column;
+				
+				.title {
+					font-size: 32rpx;
+					font-weight: 600;
+					color: #333;
+				}
+				
+				.subtitle {
+					font-size: 24rpx;
+					color: #666;
+					margin-top: 4rpx;
+				}
+			}
+		}
+	}
+	
+	.section-2 {
+		margin-top: 30rpx;
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 20rpx;
+		
+		.card {
+			position: relative;
+			height: 180rpx;
+			border-radius: 20rpx;
+			overflow: hidden;
+			
+			.card-content {
+				position: relative;
+				z-index: 2;
+				padding: 20rpx;
+				height: 100%;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+			}
+			
+			.card-title {
+				display: flex;
+				align-items: center;
+				margin-bottom: 10rpx;
+				
+				image {
+					width: 40rpx;
+					height: 40rpx;
+					margin-right: 10rpx;
+				}
+				
+				text {
+					color: #fff;
+					font-size: 28rpx;
+					font-weight: 600;
+				}
+			}
+			
+			.card-desc {
+				color: rgba(255, 255, 255, 0.9);
+				font-size: 24rpx;
+			}
+			
+			.card-bg {
+				position: absolute;
+				top: 0;
+				left: 0;
+				right: 0;
+				bottom: 0;
+				z-index: 1;
+				
+				&.bg-1 {
+					background: linear-gradient(45deg, #12c2e9, #c471ed);
+				}
+				
+				&.bg-2 {
+					background: linear-gradient(45deg, #ff6b6b, #feca57);
+				}
+			}
+		}
+	}
+	
+	.section-3 {
+		margin-top: 30rpx;
+		display: grid;
+		grid-template-columns: 2fr 1fr;
+		gap: 20rpx;
+		
+		.points-card {
+			background: linear-gradient(135deg, #a8e063 0%, #56ab2f 100%);
+			padding: 30rpx;
+			border-radius: 20rpx;
+			color: #fff;
+			
+			.points-info {
+				display: flex;
+				align-items: center;
+				margin-bottom: 10rpx;
+				
+				.points-label {
+					font-size: 28rpx;
+				}
+				
+				.points-value {
+					font-size: 40rpx;
+					font-weight: bold;
+					margin-left: 20rpx;
+				}
+			}
+			
+			.points-desc {
+				font-size: 24rpx;
+				opacity: 0.9;
+			}
+			
+			.points-btn {
+				margin-top: 20rpx;
+				display: inline-block;
+				padding: 10rpx 30rpx;
+				background: rgba(255, 255, 255, 0.2);
+				border-radius: 30rpx;
+				font-size: 24rpx;
+			}
+		}
+		
+		.qr-card {
+			background: #fff;
+			border-radius: 20rpx;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			box-shadow: 0 4rpx 20rpx rgba(0, 0, 0, 0.08);
+			
+			image {
+				width: 60rpx;
+				height: 60rpx;
+				margin-bottom: 10rpx;
+			}
+			
+			text {
+				font-size: 24rpx;
+				color: #666;
+			}
+		}
+	}
+</style>
---
+++ pages/my/member.vue
@@ -0,0 +1,333 @@
+<template>
+	<view class="container">
+		<view class="header">
+			<image src="https://static.heytea.com/weapp/images/member/me_kaitong_img_card.png" class="card-img" mode="widthFix"></image>
+			<view class="tips">开通黑金会员卡，尊享12个月专属特权</view>
+		</view>
+		<!-- 开通礼包 begin -->
+		<view class="card">
+			<view class="card-title">
+				<view class="title">入会礼遇</view>
+				<view class="subtitle">开通或续费会员即刻尊享</view>
+			</view>
+			<view class="grids">
+				<view class="grid">
+					<image src="/static/images/my/member_benefits/me_rights_icon_free.png"></image>
+					<view class="title">
+						<view>尊享礼券</view>
+						<view class="number">x2</view>
+					</view>
+				</view>
+				<view class="grid">
+					<image src="/static/images/my/member_benefits/me_rights_icon_1jia1.png"></image>
+					<view class="title">
+						<view>品鉴券</view>
+						<view class="number">x3</view>
+					</view>
+				</view>
+				<view class="grid">
+					<image src="/static/images/my/member_benefits/me_rights_icon_2jia1.png"></image>
+					<view class="title">
+						<view>臻选券</view>
+						<view class="number">x1</view>
+					</view>
+				</view>
+				<view class="grid">
+					<image src="/static/images/my/member_benefits/me_rights_icon_qingshi.png"></image>
+					<view class="title">
+						<view>悦享券</view>
+						<view class="number">x2</view>
+					</view>
+				</view>
+				<view class="grid">
+					<image src="/static/images/my/member_benefits/me_rights_icon_youxian_new.png"></image>
+					<view class="title">
+						<view>专享券</view>
+						<view class="number">x1</view>
+						<view class="badge danger">尊享特权</view>
+					</view>
+				</view>
+				<view class="grid">
+					<image src="/static/images/my/member_benefits/me_rights_icon_waimai_new.png"></image>
+					<view class="title">
+						<view>速递券</view>
+						<view class="number">x4</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		<!-- 开通礼包 end -->
+		<!-- 每月等级权益 begin -->
+		<view class="card">
+			<view class="card-title">
+				<view class="title">月度臻享</view>
+				<view class="subtitle">每月固定日期尊享专属权益</view>
+			</view>
+			<view class="card-body">
+				<view class="row">
+					<view class="title">专属通道券</view>
+					<view class="subtitle">x1/月</view>
+				</view>
+				<view class="row">
+					<view class="title">小额优惠券</view>
+					<view class="subtitle">x1/月</view>
+				</view>
+				<view class="row">
+					<view class="title">大额优惠券</view>
+					<view class="subtitle">x1/月</view>
+				</view>
+				<view class="row">
+					<view class="title">会员专享折扣</view>
+					<view class="subtitle">x1/月</view>
+				</view>
+				<view class="row">
+					<view class="title">
+						<view>会员日额外赠送10%积分</view>
+						<view class="badge primary">尊享</view>
+					</view>
+				</view>
+			</view>
+			<view class="footer">
+				<view>更多会员特权</view>
+				<image src="/static/images/common/black_arrow_right.png"></image>
+			</view>
+		</view>
+		<!-- 每月等级权益 end -->
+		<!-- 星球会员权益 begin -->
+		<view class="card">
+			<view class="card-title">
+				<view class="title">黑金特权</view>
+				<view class="subtitle">会员期间专享权益</view>
+			</view>
+			<view class="card-body">
+				<view class="row">
+					<view class="title">
+						<view>生日特惠礼遇</view>
+						<view class="badge danger">专属通道</view>
+					</view>
+					<view class="subtitle">x1</view>
+				</view>
+				<view class="row">
+					<view class="title">会员周年礼券</view>
+					<view class="subtitle">x1</view>
+				</view>
+				<view class="row">
+					<view class="title">年度庆典礼券</view>
+					<view class="subtitle">x1</view>
+				</view>
+				<view class="row">
+					<view class="title">
+						<view>积分成长加速</view>
+						<view class="badge primary">尊享</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		<!-- 星球会员权益 end -->
+		<view class="footer">
+			<view class="agreement">
+				<checkbox :checked="agree" color="#DBA871"/>
+				<view>同意<text class="text-color-primary">《黑金会员服务条款》</text></view>
+			</view>
+			<view class="bottom">
+				<view class="font-size-lg font-weight-bold ml-30">
+					年费：<text class="text-color-primary">179元</text>
+				</view>
+				<button type="primary">立即开通</button>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				agree: false
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.container {
+	height: auto;
+	padding-bottom: 200rpx;
+}
+
+.header {
+	width: 100%;
+	background-color: #f6f6f6;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 40rpx 0 0;
+	
+	.card-img {
+		width: 575rpx;
+		box-shadow: 0 10rpx 10rpx 0 rgba($color: #e3e3e3, $alpha: 0.6);
+	}
+	
+	.tips {
+		font-size: $font-size-sm;
+		color: $color-primary;
+		padding: 30rpx 0;
+	}
+}
+
+.card-title {
+	display: flex;
+	align-items: baseline;
+	margin-bottom: 20rpx;
+	
+	.title {
+		font-family: 'wenyue';
+		font-size: $font-size-lg;
+		font-weight: 500;
+	}
+	
+	.subtitle {
+		margin-left: 20rpx;
+		font-size: $font-size-base;
+		color: $text-color-assist;
+	}
+}
+
+.card {
+	width: 100%;
+	background-color: $bg-color-white;
+	padding: 30rpx 40rpx;
+	margin-bottom: 20rpx;
+	
+	.grids {
+		display: flex;
+		flex-wrap: wrap;
+		
+		.grid {
+			width: 33.3333%;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			padding: 20rpx;
+			position: relative;
+			
+			image {
+				width: 70rpx;
+				height: 70rpx;
+				margin-bottom: $spacing-row-base;
+			}
+			
+			.title {
+				font-size: $font-size-base;
+				color: $text-color-base;
+				display: flex;
+				align-items: baseline;
+				
+				.badge {
+					position: absolute;
+					top: 10rpx;
+					right: 10rpx;
+				}
+				
+				.number {
+					margin-left: 5rpx;
+					font-family: 'neutra';
+					color: $color-warning;
+				}
+			}
+		}
+	}
+	
+	.row {
+		padding: 20rpx 0;
+		display: flex;
+		justify-content: space-between;
+		font-size: $font-size-medium;
+		
+		.title {
+			display: flex;
+			align-items: center;
+			
+			.badge {
+				position: relative;
+				margin-top: -40rpx;
+				margin-left: 6rpx;
+			}
+		}
+		
+		.subtitle {
+			color: $color-primary;
+			font-weight: 500;
+		}
+	}
+	
+	.footer {
+		padding: 20rpx 0 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: $font-size-sm;
+		image {
+			width: 30rpx;
+			height: 30rpx;
+		}
+	}
+}
+
+.badge {
+	color: #FFFFFF;
+	font-size: 20rpx;
+	line-height: 1rem;
+	padding: 0 16rpx;
+	border-radius: 50rem 50rem 50rem 0;
+	
+	&.danger {
+		background-color: #d36661;
+	}
+	
+	&.primary {
+		background-color: $color-primary;
+	}
+}
+
+.footer {
+	height: 200rpx;
+	position: fixed;
+	bottom: 0;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	background-color: #FFFFFF;
+	z-index: 10;
+	
+	.agreement {
+		height: 100rpx;
+		display: flex;
+		align-items: center;
+		padding: 0 30rpx;
+		font-size: $font-size-sm;
+		color: $text-color-assist;
+		
+		uni-checkbox /deep/.uni-checkbox-input {
+			width: 34rpx;
+			height: 34rpx;
+		}
+	}
+	
+	.bottom {
+		height: 100rpx;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		border-top: 1rpx solid rgba($color: $border-color, $alpha: 0.8);
+		
+		button {
+			height: 100%;
+			font-size: $font-size-lg;
+			border-radius: 0 !important;
+			line-height: 100rpx;
+			padding: 0 100rpx;
+		}
+	}
+}
+</style>
